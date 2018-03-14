8851f02beb6b934aec7619af36d0d95a3fef8c89
coltonteefy <coltonteefy@yahoo.com>
2018-03-14 13:22:37 -0400

    add calendar event view

---
+++ src/app/calendar-event-view/calendar-event-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-event-view',
+  templateUrl: './calendar-event-view.component.html',
+  styleUrls: ['./calendar-event-view.component.css']
+})
+export class CalendarEventViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
