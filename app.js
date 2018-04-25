a87ab1f3115cfd3538d8c225b5e43b144216b4dc
coltonteefy <coltonteefy@yahoo.com>
2018-04-25 19:06:47 -0400

    changes

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.use(express.static(path.join(__dirname, '/dist')));
 const calendar = require(path.join(__dirname, '/dist/assets/calendar-events.json'));
 const pets = require(path.join(__dirname, '/dist/assets/pets.json'));
 
-// app.get('/calendar-events', (req, res) => res.json(calendar))
-// app.get('/pets', (req, res) => res.json(pets))
-// app.get('/**', (req, res, next) => res.sendFile(path.join(__dirname, '/dist/index.html')))
+app.get('/calendar-events', (req, res) => res.json(calendar))
+app.get('/pets', (req, res) => res.json(pets))
+app.get('/**', (req, res, next) => res.sendFile(path.join(__dirname, '/dist/index.html')))
 
 app.listen(process.env.PORT || 3000, console.log("Listening on port 3000"));
