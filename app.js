1cb44692268b434ff7e3a77ab4364ae24c2782a9
coltonteefy <coltonteefy@yahoo.com>
2018-04-25 20:33:28 -0400

    test

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/dist')));
-// const calendar = require(path.join(__dirname, '/dist/assets/calendar-events.json'));
-// const pets = require(path.join(__dirname, '/dist/assets/pets.json'));
+const calendar = require(path.join(__dirname, '/dist/assets/calendar-events.json'));
+const pets = require(path.join(__dirname, '/dist/assets/pets.json'));
 
-// app.get('/calendar-events', (req, res) => res.json(calendar))
-// app.get('/pets', (req, res) => res.json(pets))
+app.get('/events', (req, res) => res.json(calendar))
+app.get('/events', (req, res) => res.json(pets))
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/dist/index.html')))
 
 var port = process.env.PORT || 3000;
