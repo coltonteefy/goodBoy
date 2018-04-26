4bfb08556af563b1f404f0670543b5830c81d3cb
coltonteefy <coltonteefy@yahoo.com>
2018-04-25 22:24:37 -0400

    heroku changes

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/dist')));
-const calendar = require(path.join(__dirname, '/dist/assets/calendar-events.json'));
 const pets = require(path.join(__dirname, '/dist/assets/pets.json'));
 
-app.get('/calendar', function(req, res) {
-  res.json(calendar)
-});
 
 app.get('/pets', function(req, res) {
   res.json(pets)
 });
-// app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/dist/index.html')))
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'))
 });
